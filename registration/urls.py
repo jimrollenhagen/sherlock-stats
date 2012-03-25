629dbb78531389149826623fcce9347977405a79
Jim Rollenhagen <jim@jimrollenhagen.com>
2012-03-25 07:53:53 -0400

    register page wip, still needs to actually create user, validate what.cd username, send PM

--- registration/urls.py
+++ registration/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls.defaults import patterns, include, url
 urlpatterns = patterns('registration.views',
     url(r'login/$', 'login_page'),
     url(r'logout/$', 'logout_page'),
-    url(r'register/$', 'register')
+    url(r'register/$', 'register'),
 #    url(r'verify/$', 'verify'),
 #    url(r'profile/$', 'edit_profile')
 )
--- registration/views.py
+++ registration/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+
+from registration.models import UserProfile
 
 def login_page(request):
     if request.user.is_authenticated():
@@ -30,4 +33,46 @@ def logout_page(request):
     return redirect('/accounts/login/')
 
 def register(request):
-    return redirect('/')
+    if request.user.is_authenticated():
+        return redirect('/')
+    if not request.POST:
+        return render(request, 'registration/register.html', {})
+
+    username = request.POST['username']
+    password = request.POST['password']
+    password2 = request.POST['password2']
+    what_username = request.POST['what-username']
+
+    errors = []
+    # verify what.cd username not in use
+    dupe_user = UserProfile.objects.filter(whatcd_username=what_username).count()
+    if dupe_user:
+        errors.append('What.CD user %s already has a sherlock account' % what_username)
+
+    # verify passwords match
+    if password != password2:
+        errors.append('Passwords do not match')
+    # verify password security
+    if len(password) < 8:
+        errors.append('Passwords must be at least 8 characters')
+
+    # verify username valid and not taken
+    if not username:
+        errors.append('Username cannot be empty')
+    dupe_user = User.objects.filter(username=username).count()
+    if dupe_user:
+        errors.append('Username already taken')
+
+    # verify user with what.cd and get ID
+    # do this last so we don't make HTTP requests if not necessary
+    what_id = 154963
+
+    # return any errors
+    if errors:
+        errors = '. '.join(errors)
+        return render(request, 'registration/register.html', {'errors': errors, 'username': username, 'what_username': what_username})
+
+    # register account
+    # send PM to what.cd account
+    # return register successful
+    return render(request, 'registration/register_success.html', {})
